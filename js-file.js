8a7c6664bec0cdba93c0496ae080a91ba7e23a33
Robert Szalamin <szalamin.robert@gmail.com>
2022-05-11 16:36:39 +0200

    Object gyakorlás

--- js-file.js
+++ js-file.js
@@ -1,4 +1,4 @@
-const ujdiv = document.createElement("div");
+/*const ujdiv = document.createElement("div");
 ujdiv.classList.add("ujdiv");
 ujdiv.setAttribute("style", "border-color: black; border-style: solid; background-color: pink");
 const ujh1 = document.createElement("h1");
@@ -22,4 +22,58 @@ const ujh3 = document.createElement("h3");
 ujh3.setAttribute("style", "color: blue");
 ujh3.textContent = "I'm a blue h3!";
 
-muhaha.appendChild(ujh3);
+muhaha.appendChild(ujh3);*/
+
+/*const btn = document.querySelector('#btn');
+btn.addEventListener("click", function(e) {
+    e.target.style.background = "red";
+});
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        alert(button.id);
+    });
+});*/
+
+
+
+        //  OBJECT gyakorlás
+
+
+
+let user = {
+    name: "John",
+    surname: "Smith",
+};
+
+let player = {};
+
+/*for (let prop in user) {
+    alert(user[prop]);
+};*/
+
+function isEmpty(obj) {
+    for (let prop in obj) {
+        return false;
+    }
+    return true;
+}
+
+/*alert(isEmpty(user));
+alert(isEmpty(player));*/
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130,
+}
+
+let sumsalary = 0;
+
+for (let prop in salaries) {
+    sumsalary += salaries[prop]
+    console.log(sumsalary);
+}
+
+alert(sumsalary);
